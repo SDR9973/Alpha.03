b98439534df5102b453621761f62e3380e73b5a2
SDR9973 <shiradr95@gmail.com>
2024-12-16 00:42:10 +0200

    shira branch

--- backend/database.py
+++ backend/database.py
@@ -10,6 +10,5 @@ db_connection_string = os.getenv("DB_CONNECTION")
 
 # Create a client and connect to the database
 client = AsyncIOMotorClient(db_connection_string)
-#client = AsyncIOMotorClient("mongodb+srv://netxplore2024:3fpSZdcM9d9VmE3d@clusternet.3vkfl.mongodb.net/?retryWrites=true&w=majority&appName=ClusterNet")
 db = client["netXplore"]  # Access the 'netXplore' database
 #researchers = db["researcher"]  # Access the 'researcher' collection
